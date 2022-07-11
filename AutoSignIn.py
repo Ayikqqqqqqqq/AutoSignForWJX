604497a6986faa86bfadb83bb230bfa05881c4eb
zob128 <78293172+zob128@users.noreply.github.com>
2022-07-11 22:12:31 +0800

    Update AutoSignIn.py

--- AutoSignIn.py
+++ AutoSignIn.py
@@ -5,7 +5,8 @@ import os
 import requests
 
 headers = os.environ['qlist_header']
+url = os.environ['qlist_url']
 
 if __name__ == '__main__':
-    r = requests.get('https://www.baidu.com')
+    r = requests.get(url)
     print(r.text)
