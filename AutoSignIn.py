615a8528ae873777e80c64a916aeca02fd27ab6e
zob128 <78293172+zob128@users.noreply.github.com>
2022-07-11 22:16:31 +0800

    Update AutoSignIn.py

--- AutoSignIn.py
+++ AutoSignIn.py
@@ -8,5 +8,6 @@ headers = os.environ['qlist_header']
 url = os.environ['qlist_url']
 
 if __name__ == '__main__':
-    r = requests.get(url)
-    print(r.text)
+    #r = requests.get(url)
+    print(url)
+    print(header['Sec-Fetch-Site'])
